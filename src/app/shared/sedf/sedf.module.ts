7e6d3fddd94b26a039a1832a13dbe9368fad5f1e
hugo-dutra <hugo.dutra@hotmail.com>
2019-08-16 11:12:43 -0300

    Criada interface de gerencialmento de sincronizacao com o cards para as funcoes de sincronismo

--- src/app/shared/sedf/sedf.module.ts
+++ src/app/shared/sedf/sedf.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SedfRoutingModule } from './sedf-routing.module';
 import { GerenciarIntegracaoComponent } from './gerenciar-integracao/gerenciar-integracao.component';
 import { NbCardModule } from '@nebular/theme';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [GerenciarIntegracaoComponent],
   imports: [
     CommonModule,
     SedfRoutingModule,
+    HttpClientModule,
     NbCardModule,
   ]
 })
