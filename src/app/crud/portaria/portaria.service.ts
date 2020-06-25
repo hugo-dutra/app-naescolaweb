18dd1a652c699da3b96c4d99f25a28d513ab6f50
hugo-dutra <hugo.dutra@hotmail.com>
2020-06-25 20:57:10 -0300

    Iniciada migracao de portaria e cronograma

--- src/app/crud/portaria/portaria.service.ts
+++ src/app/crud/portaria/portaria.service.ts
@@ -28,6 +28,96 @@ export class PortariaService {
     return this.http.patch(CONSTANTES.N_HOST_API + 'portaria', portaria, headers);
   }
 
+  /**
+     * @param trm_id Id da turma on de se deseja saber a presença dos estudantes
+     * @param data Data desejada
+     * @param tipo_movimentacao Tipo demovimentação. 0 para entrada e 1 para saída.
+     */
+  public listarPresentes(trm_id: number, data: string, tipo_movimentacao: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/presentes-turma-data-movimentacao/${trm_id}/${data}/${tipo_movimentacao}`, headers);
+  }
+
+
+  public verificarFrequenciaTurmaAnoLetivo(trm_id: number, ano_letivo: number, tipo_movimentacao: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/verificar-percentual-turma/${trm_id}/${ano_letivo}/${tipo_movimentacao}`, headers);
+  }
+
+  /**
+   * Método retorna quantidade de estudantes em cada dia durante o intervalo informado
+   * @param data_inicio Periodo inicial
+   * @param data_fim Periodo final
+   * @param tipo_movimentacao tipo de movimentacao
+   */
+  public listarFrequenciaPortariaPeriodo(data_inicio: string, data_fim: string, tipo_movimentacao: number, esc_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/listar-frequencia-periodo/${data_inicio}/${data_fim}/${tipo_movimentacao}/${esc_id}`, headers);
+  }
+
+  /**
+    *
+    * @param {number} est_id
+    * @returns {Observable<any>}
+    * @memberof PortariaService
+    */
+  public listarHistoricoEntradaSaidaEstudante(est_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/historico-entrada-saida/${est_id}`, headers);
+  }
+
+
+  /**
+     *
+     * @param esc_id  Recupera o registro mais recente de passagem da portaria, subtraindo 3 dias desse valor.
+     */
+  public verificarPassagemMaisRecente(esc_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/verificar-passagem-recente/${esc_id}`, headers);
+  }
+
+
+  /**
+     *
+     * @param trm_id se o id da turma for zero, a pesquisa ocorrerá em toda a escola, caso contrário procurará pelo código da turma informado
+     * @param esc_id Código da escola
+     * @param data_inicio período inicial da consulta
+     * @param data_fim período final da consulta
+     */
+  public verificarAbsenteismoTotalPorTurma(trm_id: number, esc_id: number, data_inicio: string, data_fim: string, limite: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `frequencia-portaria/absenteismo-total-turma/${trm_id}/${esc_id}/${data_inicio}/${data_fim}/${limite}`, headers);
+  }
+
+
+  public excluir(por_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")), body: { por_id } }
+    return this.http.delete(CONSTANTES.N_HOST_API + 'portaria', headers);
+  }
+
+
+  //########################## CRONOGRAMA PORTARIA ##########################//
+
+  public inserirCronogramaPortaria(cronogramaPortaria: CronogramaPortaria): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.post(CONSTANTES.N_HOST_API + 'cronograma-portaria', cronogramaPortaria, headers);
+  }
+
+  public listarCronogramaPortaria(por_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
+    return this.http.get(CONSTANTES.N_HOST_API + `cronograma-portaria/${por_id}`, headers);
+  }
+
+  public excluirCronogramaPortaria(crp_id: number): Observable<any> {
+    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")), body: { crp_id } }
+    return this.http.delete(CONSTANTES.N_HOST_API + 'cronograma-portaria', headers);
+    //return this.http.post(CONSTANTES.HOST_API + 'excluir-cronograma-portaria', JSON.stringify({ crp_id: crp_id }), headers);
+  }
+
+
+
+
+
 
   /**********************************************************************************************************************************/
   /**********************************************************************************************************************************/
@@ -43,141 +133,19 @@ export class PortariaService {
     );
   }
 
-  /**
-   *
-   * @param trm_id Id da turma on de se deseja saber a presença dos estudantes
-   * @param data Data desejada
-   * @param tipo_movimentacao Tipo demovimentação. 0 para entrada e 1 para saída.
-   */
-  public listarPresentes(trm_id: number, data: string, tipo_movimentacao: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-
-    return this.http.post(
-      CONSTANTES.HOST_API + "listar-presentes-turma-data-movimentacao",
-      JSON.stringify({ trm_id: trm_id, data: data, tipo_movimentacao: tipo_movimentacao }),
-      headers
-    );
-  }
-
-  public verificarFrequenciaEstudanteAnoLetivo(est_id: number, ano_letivo: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-
-    return this.http.post(
-      CONSTANTES.HOST_API + "verificar-percentual-frequencia-portaria-estudante",
-      JSON.stringify({ est_id: est_id, ano_letivo: ano_letivo }),
-      headers
-    );
-  }
-
-  public verificarFrequenciaTurmaAnoLetivo(trm_id: number, ano_letivo: number, tipo_movimentacao: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-
-    return this.http.post(
-      CONSTANTES.HOST_API + "verificar-percentual-frequencia-portaria-turma",
-      JSON.stringify({ trm_id: trm_id, ano_letivo: ano_letivo, tipo_movimentacao: tipo_movimentacao }),
-      headers
-    );
-  }
-
-  /**
-   * Método retorna quantidade de estudantes em cada dia durante o intervalo informado
-   * @param data_inicio Periodo inicial
-   * @param data_fim Periodo final
-   * @param tipo_movimentacao tipo de movimentacao
-   */
-  public listarFrequenciaPortariaPeriodo(data_inicio: string, data_fim: string, tipo_movimentacao: number, esc_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-
-    return this.http.post(
-      CONSTANTES.HOST_API + "listar-frequencia-portaria-periodo",
-      JSON.stringify({ data_inicio: data_inicio, data_fim: data_fim, tipo_movimentacao: tipo_movimentacao, esc_id: esc_id }),
-      headers
-    );
-  }
-
-  /**
-   *
-   * @param {number} est_id
-   * @returns {Observable<any>}
-   * @memberof PortariaService
-   */
-  public listarHistoricoEntradaSaidaEstudante(est_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-
-    return this.http.post(
-      CONSTANTES.HOST_API + "listar-historico-entrada-saida-estudante-portaria",
-      JSON.stringify({ est_id: est_id }),
-      headers
-    );
-  }
-
-  /**
-   *
-   * @param esc_id  Recupera o registro mais recente de passagem da portaria, subtraindo 3 dias desse valor.
-   */
-  public verificarPassagemMaisRecente(esc_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-    return this.http.post(
-      CONSTANTES.HOST_API + "verificar-passagem-recente-escola",
-      JSON.stringify({ esc_id: esc_id }),
-      headers
-    );
-  }
-
-  /**
-   *
-   * @param trm_id se o id da turma for zero, a pesquisa ocorrerá em toda a escola, caso contrário procurará pelo código da turma informado
-   * @param esc_id Código da escola
-   * @param data_inicio período inicial da consulta
-   * @param data_fim período final da consulta
-   */
-  public verificarAbsenteismoTotalPorTurma(trm_id: number, esc_id: number, data_inicio: string, data_fim: string, limite: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-    return this.http.post(
-      CONSTANTES.HOST_API + "verificar-absenteismo-total-turma",
-      JSON.stringify({ trm_id: trm_id, esc_id: esc_id, data_inicio: data_inicio, data_fim: data_fim, limite: limite }),
-      headers
-    );
-  }
 
 
 
-  public excluir(por_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-    return this.http.post(
-      CONSTANTES.HOST_API + "excluir-portaria",
-      JSON.stringify({ por_id: por_id }),
-      headers
-    );
-  }
 
-  //########################## CRONOGRAMA PORTARIA ##########################//
-  public inserirCronogramaPortaria(cronogramaPortaria: CronogramaPortaria): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
 
-    return this.http.post(
-      CONSTANTES.HOST_API + 'inserir-cronograma-portaria',
-      JSON.stringify({ cronogramaPortaria: cronogramaPortaria }),
-      headers);
-  }
 
-  public excluirCronogramaPortaria(crp_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
 
-    return this.http.post(
-      CONSTANTES.HOST_API + 'excluir-cronograma-portaria',
-      JSON.stringify({ crp_id: crp_id }),
-      headers);
-  }
 
-  public listarCronogramaPortaria(por_id: number): Observable<any> {
-    const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
 
-    return this.http.post(
-      CONSTANTES.HOST_API + 'listar-cronograma-portaria',
-      JSON.stringify({ por_id: por_id }),
-      headers);
-  }
+
+
+
+
 
   //########################## FREQUENCIA DA PORTARIA ##########################//
   public inserirEntradas(por_id: number, entradas: Object[]): Observable<any> {
