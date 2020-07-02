ae1c1b0aa44794b171e696ba25380c99404fc6e4
hugo-dutra <hugo.dutra@hotmail.com>
2020-07-02 12:24:30 -0300

    Iniciada construcao de login com tratamento do token recebido

--- src/app/access/access.service.ts
+++ src/app/access/access.service.ts
@@ -10,8 +10,29 @@ export class AccessService {
 
   public logar(email: string, senha: string): Observable<any> {
     const headers = { headers: new HttpHeaders().append('Content-type', 'application/json') };
-    return this.http.post(CONSTANTES.HOST_API + 'pegar-token', { usr_email_txt: email, usr_senha_txt: senha }, headers);
+    return this.http.post(CONSTANTES.N_HOST_API + 'usuario/pegar-token', { email, senha }, headers);
+    //return this.http.post(CONSTANTES.HOST_API + 'pegar-token', { usr_email_txt: email, usr_senha_txt: senha }, headers);
   }
+
+  public listarPorEmailUsuario(email: string): Observable<any> {
+    const headers = { headers: new HttpHeaders().append('Content-type', 'application/json') };
+    return this.http.get(CONSTANTES.N_HOST_API + `escola/email-usuario/${email}`, headers);
+    //return this.http.post(CONSTANTES.HOST_API + 'escolas-email-usuario', JSON.stringify({ email: email }), headers);
+  }
+
+
+  public listarPermissoes(esc_id: number): Observable<any> {
+    debugger;
+    const headers = { headers: new HttpHeaders().append('Content-type', 'application/json').append('Authorization', localStorage.getItem('token')), };
+    return this.http.post(CONSTANTES.HOST_API + 'pegar-permissao-usuario', JSON.stringify({ esc_id: esc_id }), headers);
+    //return this.http.post(CONSTANTES.HOST_API + 'pegar-permissao-usuario', JSON.stringify({ esc_id: esc_id }), headers);
+  }
+
+
+
+  /**************************************************************************************************************************************************/
+  /**************************************************************************************************************************************************/
+  /**************************************************************************************************************************************************/
 
   public deslogar(): Observable<any> {
     /* const headers = { headers: new HttpHeaders().append("Content-type", "application/json") } */
@@ -23,13 +44,7 @@ export class AccessService {
 
   }
 
-  public listarPermissoes(esc_id: number): Observable<any> {
-    const headers = {
-      headers: new HttpHeaders().append('Content-type', 'application/json')
-        .append('Authorization', localStorage.getItem('token')),
-    };
-    return this.http.post(CONSTANTES.HOST_API + 'pegar-permissao-usuario', JSON.stringify({ esc_id: esc_id }), headers);
-  }
+
 
   public listarEscolas(limit: number, offset: number, asc: boolean): Observable<any> {
     const headers = { headers: new HttpHeaders().append('Content-type', 'application/json') };
@@ -37,9 +52,6 @@ export class AccessService {
       JSON.stringify({ limit: limit, offset: offset, asc: asc }), headers);
   }
 
-  public listarPorEmailUsuario(email: string): Observable<any> {
-    const headers = { headers: new HttpHeaders().append('Content-type', 'application/json') };
-    return this.http.post(CONSTANTES.HOST_API + 'escolas-email-usuario', JSON.stringify({ email: email }), headers);
-  }
+
 
 }
