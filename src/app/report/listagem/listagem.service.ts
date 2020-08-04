2a67607f7eead3c84dd0d5cf57b9be2847dab8a4
Hugo Alves Dutra <hugo.dutra@se.df.gov.br>
2020-08-04 12:45:32 -0300

    Migrado mecanismo gerador de listagem

--- src/app/report/listagem/listagem.service.ts
+++ src/app/report/listagem/listagem.service.ts
@@ -12,20 +12,12 @@ export class ListagemService {
 
   public listarCamposTabela(nomeTabela: string): Observable<any> {
     const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-    return this.http.post(
-      CONSTANTES.HOST_API + "listar-campos-tabela",
-      JSON.stringify({ nomeTabela: nomeTabela }),
-      headers
-    );
+    return this.http.get(CONSTANTES.N_HOST_API + `sistema/listar-campos/${nomeTabela}`, headers);
   }
 
   public listarDadosCamposSelecionados(campos: string, esc_id: number, ordem: string): Observable<any> {
     const headers = { headers: new HttpHeaders().append("Content-type", "application/json").append("Authorization", localStorage.getItem("token")) }
-    return this.http.post(
-      CONSTANTES.HOST_API + "listar-dados-campos-tabela",
-      JSON.stringify({ campos: campos, esc_id: esc_id, ordem: ordem }),
-      headers
-    );
+    return this.http.get(CONSTANTES.N_HOST_API + `sistema/dados-campos-tabela/${campos}/${esc_id}/${ordem}`, headers);
   }
 
 }
